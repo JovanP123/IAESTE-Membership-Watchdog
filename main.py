07fb2231b76ca9506d8f277ef4aeecbf3887e855
JovanP123 <132947823+JovanP123@users.noreply.github.com>
2025-08-06 20:17:08 +0200

    Initial commit
    prvo dodavanje na kodot

---
+++ main.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import json
+import requests
+from io import BytesIO
+
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+config_path = "config.json"
+config_loaded_json = None
+
+succesful_response_status_code = 200
+print("Reading config file...")
+try:
+    f = open(config_path, "r")
+    config_loaded_json = json.load(f)
+except FileNotFoundError:
+    print(f"Can't find {config_path}")
+    exit()
+
+def get_excel(url, username, password):
+    session = requests.Session()
+    session.auth = (username, password)
+    response = session.get(url)
+
+    if response.status_code == succesful_response_status_code:
+        return BytesIO(response.content)
+    else:
+        raise Exception(f"Failed to fetch file: {response.status_code} - {response.text}")
+def excel_to_array(excelFile):
+    df = pd.read_excel(excelFile, engine="openpyxl")
+    return df.values.tolist()
+def send_mail_from_outlook(recipient_email, subject, body):
+    sender_email = config_loaded_json["outlook_email"]
+    app_password = config_loaded_json["outlook_password"]
+
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = recipient_email
+    message["Subject"] = subject
+    message.attach(MIMEText(body, "plain"))
+
+    try:
+        with smtplib.SMTP("smtp.office365.com", 587) as server:
+            server.starttls()
+            server.login(sender_email, app_password)
+            server.sendmail(sender_email, recipient_email, message.as_string())
+            print(f"Email to {recipient_email} on subject {subject} has been sent successfully")
+    except Exception as e:
+        print(f"FAILED: {e}")
+
+print("Getting excel file from sharepoint...")
+excel_file = get_excel(config_loaded_json["sharepoint_excel_url"],config_loaded_json["sharepoint_username"],config_loaded_json["sharepoint_password"])
